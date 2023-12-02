2536fd6f71083286948296065ebb87d8d25231b1
Ather Fawaz <atherfawaz@gmail.com>
2023-12-02 17:55:25 +0400

    Q1

---
+++ day1/sol.go
@@ -0,0 +1,60 @@
+package day1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func isASCIIDigit(char byte) bool {
+	return char >= '0' && char <= '9'
+}
+
+func SolveDay1Q1() {
+
+	file, err := os.Open("day1/input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	total := 0
+	start := 0
+	last_ix := 0
+	first := -1
+	last := -1
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		first = -1
+		last = -1
+		start = 0
+		last_ix = len(line) - 1
+		for start <= last_ix {
+			if isASCIIDigit(line[start]) {
+				if first == -1 {
+					first = int(line[start] - '0')
+				} else {
+					last = int(line[start] - '0')
+				}
+			}
+			start++
+		}
+		if first != -1 {
+			total += first * 10
+		}
+		if last != -1 {
+			total += last
+		} else {
+			total += first
+		}
+		// fmt.Printf("For the string %s, the first is %d, the last is %d, and the output is %d\n", line, first, last, total)
+	}
+	fmt.Printf("Final output: %d", total)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"aoc/day1"
+)
+
+func main() {
+	day1.SolveDay1Q1()
+}
